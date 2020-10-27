fd63131bad8a2c5b0b6579889b91ab7d1b356606
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-27 21:49:04 +0100

    Added effect on corresponding circles (full-empty)

--- action.js
+++ action.js
@@ -12,15 +12,23 @@ $(document).ready(function() {
   $('.prev > i').click(function() {
     // Storing current picture position (of ".active") in a variable
     var current_img = $('.active');
+    // Storing current circle position (full circle) in a variable
+    var current_circle = $('.circles > .fas');
     // Hiding current picture
     current_img.removeClass('active');
+    // Emptying the current full circle
+    current_circle.toggleClass('fas far');
     // Checking if there is any previous picture
     if (current_img.prev().length) {
       // Showing previous picture
       current_img.prev().addClass('active');
+      // Selecting the corresponding previous circle (make it full)
+      current_circle.prev().toggleClass('fas far');
     } else {
       // Showing last picture (restart from the end)
       $('.slides > img:last-child').addClass('active');
+      // Selecting the corresponding last circle (make it full)
+      $('.circles > i:last-child').toggleClass('fas far');
     }
   });
 
@@ -29,14 +37,23 @@ $(document).ready(function() {
   $('.next > i').click(function() {
     // Storing current picture position (of ".active") in a variable
     var current_img = $('.active');
+    // Storing current circle position (full circle) in a variable
+    var current_circle = $('.circles > .fas');
     // Hiding current picture
     current_img.removeClass('active');
+    // Emptying the current full circle
+    current_circle.toggleClass('fas far');
+    // Checking if there is any following picture
     if(current_img.next().length) {
       // Showing following picture
       current_img.next().addClass('active');
+      // Selecting the corresponding following circle (make it full)
+      current_circle.next().toggleClass('fas far');
     } else {
       // Showing the first picture (restart from the beginning)
       $('.slides > img:first-child').addClass('active');
+      // Selecting the corresponding first circle (make it full)
+      $('.circles > i:first-child').toggleClass('fas far');
     }
   });
 });
